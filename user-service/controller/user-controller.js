9cd4b019c8c63b8c752866dfb7ba4cf0871c1ed2
KengXIII <kengi0813@gmail.com>
2022-09-02 00:34:53 +0800

    Add check for duplicate username

--- user-service/controller/user-controller.js
+++ user-service/controller/user-controller.js
@@ -5,17 +5,17 @@ export async function createUser(req, res) {
         const { username, password } = req.body;
         if (username && password) {
             const resp = await _createUser(username, password);
-            console.log(resp);
-            if (resp.err) {
-                return res.status(400).json({message: 'Could not create a new user!'});
+            // console.log(resp);
+            if (resp.err) { 
+                return res.status(400).json({message: resp.message});
             } else {
                 console.log(`Created new user ${username} successfully!`)
-                return res.status(201).json({message: `Created new user ${username} successfully!`});
+                return res.status(201).json({message: resp.message});
             }
         } else {
             return res.status(400).json({message: 'Username and/or Password are missing!'});
         }
     } catch (err) {
-        return res.status(500).json({message: 'Database failure when creating new user!'})
+        return res.status(500).json({message: 'Unexpected database failure when creating new user!'})
     }
 }
--- user-service/model/user-orm.js
+++ user-service/model/user-orm.js
@@ -1,14 +1,27 @@
-import { createUser } from './repository.js';
+import { createUser } from "./repository.js";
 
 //need to separate orm functions from repository to decouple business logic from persistence
 export async function ormCreateUser(username, password) {
-    try {
-        const newUser = await createUser({username, password});
-        newUser.save();
-        return true;
-    } catch (err) {
-        console.log('ERROR: Could not create new user');
-        return { err };
-    }
-}
+  try {
+    const newUser = await createUser({ username, password });
+    await newUser.save();
+    return {
+      err: null,
+      message: `Created new user ${username} successfully!`,
+    };
+  } catch (err) {
+    let logMessage;
 
+    if (err && err.code === 11000) {
+      //duplicate key
+      logMessage = `Username ${username} has been used!`;
+    }
+
+    console.log(logMessage);
+
+    return {
+      err: err,
+      message: logMessage,
+    };
+  }
+}
