b5f8e131020a014514644cb9a9ba6203642268a9
LAPTOP-5UFUBL5J\tohzh <toh.zhan.qing@gmail.com>
2022-09-16 18:12:36 +0800

    main page fe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
 import SignupPage from './components/SignupPage';
+import MainPage from './components/MainPage';
 import {Box} from "@mui/material";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
                     <Routes>
                         <Route exact path="/" element={<Navigate replace to="/signup" />}></Route>
                         <Route path="/signup" element={<SignupPage/>}/>
+                        <Route path="/main" element={<MainPage/>}/>
                     </Routes>
                 </Router>
             </Box>
