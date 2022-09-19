cb5d2beb0230b2947f277fb359e34835e42ab606
KengXIII <kengi0813@gmail.com>
2022-09-19 17:26:42 +0800

    Add new route for deleteUser

--- user-service/routes.js
+++ user-service/routes.js
@@ -6,9 +6,10 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors()) // config cors so that front-end can use
 app.options('*', cors())
-import { createUser } from './controller/user-controller.js';
+import { createUser, deleteUser } from './controller/user-controller.js';
 
 const routes = express.Router()
     routes.post('/signup', createUser)
+    routes.post('/deleteuser', deleteUser)
 
 export default routes 
