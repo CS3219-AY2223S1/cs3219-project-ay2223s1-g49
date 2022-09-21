80adcc4be4f2533f6213f364c7388f82c21fc537
KengXIII <kengi0813@gmail.com>
2022-09-21 14:22:40 +0800

    Refactor routes in index.js to new file

--- user-service/routes.js
+++ user-service/routes.js
@@ -1,14 +1,21 @@
 import express from 'express';
 import cors from 'cors';
+import { auth, block, allow } from './middleware.js'
+import cookieParser from 'cookie-parser';
 
 const app = express();
 app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 app.use(express.json())
 app.use(cors()) // config cors so that front-end can use
 app.options('*', cors())
-import { createUser } from './controller/user-controller.js';
+import { createUser, authUser, checkUser, logout, validateToken } from './controller/user-controller.js';
 
 const routes = express.Router()
-    routes.post('/signup', createUser)
+routes.post('/signup', allow, createUser)
+routes.post('/login', allow, authUser)
+routes.get('/check', block, checkUser)
+routes.post('/logout', auth, logout)
+routes.post('/validate-token', auth, validateToken)
 
 export default routes 
