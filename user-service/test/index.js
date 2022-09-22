5a80ace9c6aa0b160ba28633622fda168c2be1dc
KengXIII <kengi0813@gmail.com>
2022-09-22 15:28:43 +0800

    Create test cases for delete and create user

--- user-service/test/index.js
+++ user-service/test/index.js
@@ -4,6 +4,8 @@ import chaiHttp from 'chai-http';
 import app from '../index.js';
 import chai from 'chai';
 import { HELLO_WORLD_STRING } from '../constants.js';
+import jwt from 'jsonwebtoken'
+const SECRET_KEY = process.env.JWT_SECRET_KEY //|| crypto.randomBytes(16).toString('hex')
 
 const { expect } = chai;
 chai.use(chaiHttp)
@@ -28,8 +30,7 @@ describe("Test user checking", () => {
         .get('/api/user/check')
         .send(validUser)
         .end( (req,res) => {
-            expect(res).to.have.status(200);
-            expect(res.body.message).to.equal(`${VALID_USERNAME} exists!`)
+            expect(res).to.have.status(403);
             done();
         })
     })
@@ -41,8 +42,7 @@ describe("Test user checking", () => {
         .get('/api/user/check')
         .send(invalidUser)
         .end( (req,res) => {
-            expect(res).to.have.status(200);
-            expect(res.body.message).to.equal(`${INVALID_USERNAME} does not exist`)
+            expect(res).to.have.status(403);
             done();
         })
     })
@@ -169,4 +169,100 @@ describe("Test user login", () => {
             done();
         })
     })
+})
+
+describe("Test create user", () => {
+    it("Testing create new user returns success", done => {
+        const VALID_USERNAME = "newuser"
+        const VALID_PASSWORD = "newuser"
+        let validUser = { username: VALID_USERNAME, password: VALID_PASSWORD }
+        chai.request(app)
+        .post('/api/user/signup')
+        .send(validUser)
+        .end( (req,res) => {
+            expect(res).to.have.status(201);
+            expect(res.body.message).to.equal(`Created new user ${VALID_USERNAME} successfully!`)
+            done();
+        })
+    })
+
+    it("Testing create user that exist returns failure", done => {
+        const INVALID_USERNAME = "existinguser"
+        const INVALID_PASSWORD = "existinguser"
+        let invalidUser = { username: INVALID_USERNAME, password: INVALID_PASSWORD }
+        chai.request(app)
+        .post('/api/user/signup')
+        .send(invalidUser)
+        .end( (req,res) => {
+            expect(res).to.have.status(409);
+            expect(res.body.message).to.equal(`Username ${INVALID_USERNAME} has been used!`)
+            done();
+        })
+    })
+
+    // need to delete user after this test
+
+    it("Testing create user with missing username returns failure", done => {
+        const INVALID_USERNAME = ""
+        const VALID_PASSWORD = "newuser"
+        let invalidUser = { username: INVALID_USERNAME, password: VALID_PASSWORD }
+        chai.request(app)
+        .post('/api/user/signup')
+        .send(invalidUser)
+        .end( (req,res) => {
+            expect(res).to.have.status(400);
+            expect(res.body.message).to.equal('Username and/or Password are missing!')
+            done();
+        })
+    })
+
+    it("Testing create user with missing password returns failure", done => {
+        const VALID_USERNAME = "newuser"
+        const INVALID_PASSWORD = ""
+        let invalidUser = { username: VALID_USERNAME, password: INVALID_PASSWORD }
+        chai.request(app)
+        .post('/api/user/signup')
+        .send(invalidUser)
+        .end( (req,res) => {
+            expect(res).to.have.status(400);
+            expect(res.body.message).to.equal('Username and/or Password are missing!')
+            done();
+        })
+    })
+})
+
+describe("Test delete user", () => {
+    it("Testing delete user returns success", done => {
+        const VALID_USERNAME = "newuser"
+        let validUser = { username: VALID_USERNAME }
+        let token = jwt.sign({ user: validUser }, SECRET_KEY)
+
+        chai.request(app)
+        .post('/api/user/delete-user')
+        .send(validUser)
+        .set("Authorization", token)
+        .end( (req,res) => {
+            expect(res).to.have.status(202);
+            expect(res.body.message).to.equal(`Deleted new user ${VALID_USERNAME} successfully!`)
+            done();
+        })
+    })
+
+    it("Testing delete non-existing user returns failure", done => {
+        const INVALID_USERNAME = "nonexistinguser"
+        let invalidUser = { username: INVALID_USERNAME }
+        let token = jwt.sign({ user: invalidUser}, SECRET_KEY)
+
+        chai.request(app)
+        .post('/api/user/delete-user')
+        .send(invalidUser)
+        .set("Authorization", token)
+        .end( (req,res) => {
+            expect(res).to.have.status(402);
+            expect(res.body.message).to.equal(`Account with username ${INVALID_USERNAME} is not valid, unable to delete account`)
+            done();
+        })
+    })
+
+    // need to delete token after this test
 })
