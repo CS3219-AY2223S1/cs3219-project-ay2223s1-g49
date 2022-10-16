06f191be709c82c35e86800b4e9e92e90165e9ce
simjunheng <sjh_98@hotmail.com>
2022-10-17 01:33:49 +0800

    Matching Page frontend

---
+++ frontend/src/components/MatchingPage.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import {
+  Button,
+  Typography
+} from "@mui/material";
+import {makeStyles} from "@material-ui/core/styles";
+import {
+  Container,
+  Carousel,
+  Row,
+  Col,
+  Image,
+  Card
+} from "react-bootstrap";
+import RocketLaunchIcon from '@mui/icons-material/RocketLaunch';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import rocketImage from "../images/rocket.gif";
+import greenPlanet from "../images/green-planet.png"
+import redPlanet from "../images/red-planet.png"
+import yellowPlanet from "../images/yellow-planet.png"
+import ParticlesComponent from "./Particles.js"
+
+const useStyles = makeStyles({
+  leftContainer: {
+    backgroundColor: '#0B034A',
+    height: '100vh',
+  },
+  rightContainer: {
+    backgroundColor: '#FFFFFF',
+    height: '100vh',
+  },
+  planetImage: {
+    borderRadius: 8,
+    height: '100%',
+    width: '100%'
+  }
+})
+
+function MatchingPage() {
+    const classes = useStyles();
+    const username = "Sim Jun Heng";
+    return (
+    <Container fluid>
+      <Row>
+        <Col className={classes.leftContainer} xs={4}>
+            <div class="d-flex justify-content-center mt-4">
+              <Image fluid src={rocketImage} style={{height: '100%', width: '100%'}}/>
+            </div>
+            <div class="d-flex justify-content-center mt-3">
+              <Card class="text-center" style={{ width: '18rem' }}>
+                <Card.Body>
+                  <Card.Title>PEER-ER {username}</Card.Title>
+                  <Card.Text>
+                    Ready to take on some challenges?
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+            <div class="d-flex justify-content-center mt-4">
+              <Button
+                variant="outlined"
+                style={{
+                    borderRadius: 35,
+                    backgroundColor: "#21b6ae",
+                    padding: "10px 30px",
+                    fontSize: "15px"
+                }}> LOGOUT </Button>
+            </div>
+        </Col>
+        <Col className={classes.rightContainer} xs={8}>
+          <div class="mt-2">
+            <Typography style={{ flexGrow: 1, fontWeight: 600, fontSize: 40}} color="#424242">
+              Welcome to Peerprep
+              <RocketLaunchIcon style={{marginLeft: 6, marginBottom: 4,fontSize: 40}}/>
+            </Typography>
+          </div>
+          <div class="mt-2">
+            <Typography style={{ flexGrow: 1, fontWeight: 600, fontSize: 25}} color="#424242">
+              Find a Coding Partner
+            </Typography>
+            <Typography style={{ flexGrow: 1, fontSize: 14}} color="#424242">
+              Collaborate with another student and practice for your interviews together.
+            </Typography>
+          </div>
+          <div class="mt-2 d-flex flex-row">
+            <Card
+              class="overflow-hidden"
+              style={{ width: '33%', height: '14vw', borderColor: '#FFFFFF'}}
+              onClick={() => alert("Clicked on Easy Mode")}>
+              <Card.Img src={greenPlanet} alt="Card image" className={classes.planetImage}/>
+            </Card>
+            <Card
+              class="overflow-hidden"
+              style={{ width: '33%', height: '14vw', marginLeft: 10, borderColor: '#FFFFFF'}}
+              onClick={() => alert("Clicked on Intermediate Mode")}>
+              <Card.Img src={yellowPlanet} alt="Card image" className={classes.planetImage}/>
+            </Card>
+            <Card
+              class="overflow-hidden"
+              style={{ width: '33%', height: '14vw', marginLeft: 10, borderColor: '#FFFFFF'}}
+              onClick={() => alert("Clicked on Hard Mode")}>
+              <Card.Img src={redPlanet} alt="Card image" className={classes.planetImage} />
+            </Card>
+          </div>
+        </Col>
+      </Row>
+      <ParticlesComponent />
+    </Container>
+    );
+}
+
+export default MatchingPage;
---
+++ frontend/src/components/Particles.js
@@ -0,0 +1,42 @@
+import Particles from "react-tsparticles";
+import { loadSlim } from "tsparticles-slim";
+import { loadFull } from "tsparticles";
+import { useMemo, useCallback } from "react"
+
+function ParticlesComponent() {
+  const options = useMemo(() => {
+    return {
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "push"
+          }
+        }
+      },
+      particles: {
+        move: {
+          enable: true,
+          speed: {min: 1, max: 2}
+        },
+        size: {
+          value: {min: 1, max: 3}
+        },
+        opacity: {
+          value: {min: 0.3, max: 0.8}
+        }
+      }
+    };
+  }, []);
+
+  const particlesInit = useCallback((engine) => {
+    loadSlim(engine);
+  }, []);
+
+  return(
+    <Particles init={particlesInit} options={options} />
+  );
+
+}
+
+export default ParticlesComponent;
