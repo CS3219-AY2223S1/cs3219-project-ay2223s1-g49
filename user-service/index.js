beeaec3caf519c1105d125bd15197f10033c12b1
alexteo98 <alexteo98@gmail.com>
2022-10-27 16:37:15 +0800

    update user-service port env var

--- user-service/index.js
+++ user-service/index.js
@@ -9,6 +9,7 @@ app.use(express.json())
 app.use(cors()) // config cors so that front-end can use
 app.options('*', cors())
 import cookieParser from 'cookie-parser';
+const port = process.env.PORT || 8000
 
 // Controller will contain all the User-defined Routes
 app.use('/api/user', routes).all((_, res) => {
@@ -17,6 +18,6 @@ app.use('/api/user', routes).all((_, res) => {
 
 })
 
-app.listen(8000, () => console.log('user-service listening on port 8000'));
+app.listen(port, () => console.log('user-service listening on port 8000'));
 
 export default app
