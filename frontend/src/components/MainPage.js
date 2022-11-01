ec49a55572bf2f08b48f6f32a3a38f05e25c9be2
KengXIII <kengi0813@gmail.com>
2022-11-01 21:42:49 +0800

    Implement a page for admin user to manage questions

--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -1,155 +1,193 @@
-import Cookies from 'universal-cookie';
-import { useState, useEffect } from 'react'
-import { useNavigate } from "react-router-dom"
+import Cookies from "universal-cookie";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { URL_USER_LOGOUT, URL_USER_DELETE, URL_USER_GET_USERNAME, URL_USER_CHANGE_PASSWORD } from "../configs";
-import { STATUS_CODE_DELETE_USER_SUCCESS, STATUS_CODE_CHANGE_PASSWORD_SUCCESS, STATUS_CODE_CHANGE_PASSWORD_INVALID_CRED } from "../constants";
-import { getCollabDetails} from "../client/client.js"
+import {
+    URL_USER_LOGOUT,
+    URL_USER_DELETE,
+    URL_USER_GET_USERNAME,
+    URL_USER_CHANGE_PASSWORD,
+} from "../configs";
+import {
+    STATUS_CODE_DELETE_USER_SUCCESS,
+    STATUS_CODE_CHANGE_PASSWORD_SUCCESS,
+    STATUS_CODE_CHANGE_PASSWORD_INVALID_CRED,
+} from "../constants";
+import { getCollabDetails } from "../client/client.js";
 import LoginPage from "./LoginPage";
 import MatchingPage from "./MatchingPage";
 import validateToken from "./validate-token";
+import QuestionAdminPage from "./QuestionAdminPage";
 
 function MainPage() {
     const navigate = useNavigate();
-    const cookies = new Cookies()
-    const [isLogin, setIsLogin] = useState(false)
-    const [token, setToken] = useState(cookies.get('access token'))
-    const [username, setUsername] = useState("")
-    const [oldpassword, setOldPw] = useState("")
-    const [newpassword, setNewPw] = useState("")
-    const [verifynewpassword, setVerifyNewPw] = useState("")
-
+    const cookies = new Cookies();
+    const [isLogin, setIsLogin] = useState(false);
+    const [token, setToken] = useState(cookies.get("access token"));
+    const [username, setUsername] = useState("");
+    const [oldpassword, setOldPw] = useState("");
+    const [newpassword, setNewPw] = useState("");
+    const [verifynewpassword, setVerifyNewPw] = useState("");
 
     if (!token) {
         return <LoginPage setToken={setToken} />;
     }
 
-    validateToken(token).then(tokenValid => {
-        console.log("verifying token")
+    validateToken(token).then((tokenValid) => {
+        console.log("verifying token");
         if (!tokenValid) {
-            console.log("invalid token")
-            cookies.remove('access token')
-            setIsLogin(false)
+            console.log("invalid token");
+            cookies.remove("access token");
+            setIsLogin(false);
         } else {
-            console.log("valid token")
-            setIsLogin(true)
-            initialiseUsername()
-            getCollabDetails(username)
-            const details = JSON.parse(localStorage.getItem("globalVariable"))
+            console.log("valid token");
+            setIsLogin(true);
+            initialiseUsername();
+            getCollabDetails(username);
+            const details = JSON.parse(localStorage.getItem("globalVariable"));
             if (details !== null) {
-              navigate("/collab");
-            }         
+                navigate("/collab");
+            }
         }
-    })
+    });
 
     const initialiseUsername = () => {
         const instance = createAxiosHeader();
-        instance.post(URL_USER_GET_USERNAME)
-            .then(res => { setUsername(res.data.username) })
-            .catch((err) => { console.log("Error getting username from cookie: ", err.toJSON()) })
-    }
+        instance
+            .post(URL_USER_GET_USERNAME)
+            .then((res) => {
+                setUsername(res.data.username);
+            })
+            .catch((err) => {
+                console.log(
+                    "Error getting username from cookie: ",
+                    err.toJSON()
+                );
+            });
+    };
 
     const handleLogout = async () => {
         const instance = createAxiosHeader();
-        await instance.post(URL_USER_LOGOUT)
-            .then(res => { })
-            .catch((err) => { })
-        cookies.remove('access token')
-        localStorage.removeItem('globalVariable');
+        await instance
+            .post(URL_USER_LOGOUT)
+            .then((res) => {})
+            .catch((err) => {});
+        cookies.remove("access token");
+        localStorage.removeItem("globalVariable");
         //setIsLogin(false)
-        window.location.reload(false)
-    }
+        window.location.reload(false);
+    };
 
     const handleDeleteAccount = async () => {
         const instance = createAxiosHeader();
         if (!username || username === "") {
-            console.log("No username initialised, please relogin...")
+            console.log("No username initialised, please relogin...");
             return;
         }
 
-        await instance.post(URL_USER_DELETE, { username })
-            .then(res => {
+        await instance
+            .post(URL_USER_DELETE, { username })
+            .then((res) => {
                 if (res && res.status === STATUS_CODE_DELETE_USER_SUCCESS) {
-                    console.log("Delete user successful!")
+                    console.log("Delete user successful!");
                 } else {
-                    console.log("Delete user failure!")
+                    console.log("Delete user failure!");
                 }
             })
             .catch((err) => {
-                console.log("Error at handleDeleteAccount: ", err.toJSON())
-            })
-        handleLogout()
-    }
+                console.log("Error at handleDeleteAccount: ", err.toJSON());
+            });
+        handleLogout();
+    };
 
     const handleChangePassword = async () => {
-
-        if (!newpassword || newpassword == "" || !oldpassword || oldpassword == "" || !verifynewpassword || verifynewpassword == "") {
+        if (
+            !newpassword ||
+            newpassword == "" ||
+            !oldpassword ||
+            oldpassword == "" ||
+            !verifynewpassword ||
+            verifynewpassword == ""
+        ) {
             console.log("All fields are required");
             alert("All fields are required.");
             return;
         }
 
         if (newpassword !== verifynewpassword) {
-            console.log("New passwords do not match. Please retype your new passwords.");
-            alert("New passwords do not match. Please retype your new passwords.");
+            console.log(
+                "New passwords do not match. Please retype your new passwords."
+            );
+            alert(
+                "New passwords do not match. Please retype your new passwords."
+            );
             resetPasswordFields();
             return;
         }
 
         const instance = createAxiosHeader();
-        await instance.post(URL_USER_CHANGE_PASSWORD,
-            {
+        await instance
+            .post(URL_USER_CHANGE_PASSWORD, {
                 username: username,
                 oldpassword: oldpassword,
-                newpassword: newpassword
-            }).then(res => {
+                newpassword: newpassword,
+            })
+            .then((res) => {
                 if (res && res.status === STATUS_CODE_CHANGE_PASSWORD_SUCCESS) {
-                    console.log("Password successfully changed!")
-                    alert("Password successfully changed!")
-                    resetPasswordFields()
+                    console.log("Password successfully changed!");
+                    alert("Password successfully changed!");
+                    resetPasswordFields();
                 } else {
-                    console.log("Unable to change password")
-                    alert("Unable to change password, please try again later")
-                    resetPasswordFields()
+                    console.log("Unable to change password");
+                    alert("Unable to change password, please try again later");
+                    resetPasswordFields();
                 }
             })
             .catch((err) => {
-                if (err.response.status == STATUS_CODE_CHANGE_PASSWORD_INVALID_CRED) {
-                    console.log("Unable to verify current password. Please try again.")
-                    alert("Unable to verify current password. Please try again.")
-                    resetPasswordFields()
+                if (
+                    err.response.status ==
+                    STATUS_CODE_CHANGE_PASSWORD_INVALID_CRED
+                ) {
+                    console.log(
+                        "Unable to verify current password. Please try again."
+                    );
+                    alert(
+                        "Unable to verify current password. Please try again."
+                    );
+                    resetPasswordFields();
+                } else {
+                    console.log("Error at handleChangePW: ", err.toJSON());
                 }
-                else {
-                    console.log("Error at handleChangePW: ", err.toJSON())
-                }
-            })
-    }
+            });
+    };
 
     const resetPasswordFields = () => {
-        setOldPw("")
-        setNewPw("")
-        setVerifyNewPw("")
-
-    }
+        setOldPw("");
+        setNewPw("");
+        setVerifyNewPw("");
+    };
 
     function createAxiosHeader() {
-        const jwt = cookies.get('access token');
+        const jwt = cookies.get("access token");
         const instance = axios.create({
             headers: {
-                'Authorization': jwt
-            }
+                Authorization: jwt,
+            },
         });
         return instance;
     }
 
-    return (
-        !isLogin
-            ? <LoginPage setToken={setToken} />
-            : <MatchingPage
-                handleLogout={handleLogout}
-                handleDeleteAccount={handleDeleteAccount}
-                username={username} />
-    )
+    return !isLogin ? (
+        <LoginPage setToken={setToken} />
+    ) : username === "admin" ? (
+        <QuestionAdminPage />
+    ) : (
+        <MatchingPage
+            handleLogout={handleLogout}
+            handleDeleteAccount={handleDeleteAccount}
+            username={username}
+        />
+    );
 }
 
 export default MainPage;
---
+++ frontend/src/components/QuestionAdminPage.js
@@ -0,0 +1,238 @@
+import { Button, Grid, Tab, Tabs, TextField } from "@mui/material";
+import React, { useState } from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
+import makeStyles from "@mui/styles/makeStyles";
+import Alert from "@mui/material/Alert";
+import Snackbar from "@mui/material/Snackbar";
+import axios from "axios";
+
+const useStyles = makeStyles({
+    edit: {
+        "&:hover": {
+            backgroundColor: "#DADADA",
+        },
+    },
+});
+
+function QuestionAdminPage() {
+    const [difficulty, setDifficulty] = useState();
+    const [question, setQuestion] = useState();
+    const [answer, setAnswer] = useState();
+    const [editMode, setEditMode] = useState({
+        current: 0,
+        create: true,
+        edit: false,
+    });
+    const [questionList, setQuestionList] = useState([
+        {
+            _id: "63610e7bc1ee88fecc8f76ba",
+            difficulty: "hard",
+            content: " cool  ",
+            __v: 0,
+        },
+        {
+            _id: "63611037c4e7a180544bfe0d",
+            difficulty: "hard",
+            content: " cool2  ",
+            __v: 0,
+        },
+        {
+            _id: "6361103cc4e7a180544bfe0f",
+            difficulty: "hard",
+            content: " cool3  ",
+            __v: 0,
+        },
+    ]);
+    const [notificationStatus, setNotificationStatus] = useState({
+        success: false,
+        failure: false,
+    });
+
+    const classes = useStyles();
+    const clearInput = () => {
+        setDifficulty("");
+        setQuestion("");
+        setAnswer("");
+    };
+
+    return (
+        <Grid container direction="row">
+            <Grid id="left" item xs={6} p={6}>
+                <Tabs
+                    value={editMode.current}
+                    onChange={(event, newValue) => {
+                        setEditMode({
+                            current: newValue,
+                            ...editMode.create,
+                            ...editMode.edit,
+                        });
+                        console.log(editMode);
+                    }}
+                >
+                    <Tab value={0} label="Create">
+                        <p>Create</p>
+                    </Tab>
+                    <Tab value={1} label="Edit" disabled={!editMode.edit}>
+                        <p>Edit</p>
+                    </Tab>
+                </Tabs>
+                <div className="my-5">
+                    <div>Difficulty</div>
+                    <TextField
+                        value={difficulty}
+                        placeholder="Difficulty level"
+                        variant="filled"
+                        onChange={(e) => setDifficulty(e.target.value)}
+                    ></TextField>
+                </div>
+                <div className="my-5">
+                    <div>Question</div>
+                    <TextField
+                        multiline
+                        rows={5}
+                        value={question}
+                        fullWidth
+                        placeholder="The content of the question"
+                        variant="filled"
+                        onChange={(e) => setQuestion(e.target.value)}
+                    />
+                </div>
+                <div className="my-5">
+                    <div>Answer</div>
+
+                    <TextField
+                        multiline
+                        rows={5}
+                        value={answer}
+                        fullWidth
+                        placeholder="The suggested solution to the question"
+                        variant="filled"
+                        onChange={(e) => setAnswer(e.target.value)}
+                    />
+                </div>
+                <Button
+                    color="primary"
+                    onClick={() => {
+                        axios
+                            .create()
+                            .post(
+                                "http://localhost:7000/service/create/question",
+                                {
+                                    difficulty: difficulty,
+                                    content: question,
+                                    answer: answer,
+                                }
+                            )
+                            .then((res) => {
+                                if (res.status === 200) {
+                                    setNotificationStatus({
+                                        ...notificationStatus,
+                                        success: true,
+                                    });
+                                    clearInput();
+                                }
+                            })
+                            .catch(() => {
+                                setNotificationStatus({
+                                    ...notificationStatus,
+                                    failure: true,
+                                });
+                            });
+                    }}
+                >
+                    Create
+                </Button>
+                <Button color="secondary" onClick={clearInput}>
+                    Clear
+                </Button>
+            </Grid>
+            <Grid id="right" item xs={6} p={6}>
+                {questionList.map((item, idx) => (
+                    <Grid
+                        direction="row"
+                        container
+                        sx={{
+                            my: 5,
+                            border: 1,
+                            maxHeight: "80vh",
+                            overflow: "auto",
+                        }}
+                    >
+                        <Grid item xs={5}>
+                            {item._id}
+                        </Grid>
+                        <Grid item xs={6}>
+                            {item.difficulty}
+                        </Grid>
+                        <Grid item xs={1}>
+                            <div
+                                className={classes.edit}
+                                style={{
+                                    textAlign: "center",
+                                    borderRadius: "50px",
+                                }}
+                            >
+                                <DeleteIcon
+                                    sx={{
+                                        transform: "scale(1.2)",
+                                    }}
+                                />
+                            </div>
+                        </Grid>
+                        <Grid item xs={12}>
+                            {item.content}
+                        </Grid>
+                    </Grid>
+                ))}
+            </Grid>
+            <Snackbar
+                open={notificationStatus.success}
+                autoHideDuration={5000}
+                onClose={() => {
+                    setNotificationStatus({
+                        ...notificationStatus,
+                        success: false,
+                    });
+                }}
+                anchorOrigin={{ vertical: "top", horizontal: "right" }}
+            >
+                <Alert
+                    severity="success"
+                    onClose={() => {
+                        setNotificationStatus({
+                            ...notificationStatus,
+                            success: false,
+                        });
+                    }}
+                >
+                    Question created succesfully!
+                </Alert>
+            </Snackbar>
+            <Snackbar
+                open={notificationStatus.failure}
+                autoHideDuration={5000}
+                onClose={() => {
+                    setNotificationStatus({
+                        ...notificationStatus,
+                        failure: false,
+                    });
+                }}
+                anchorOrigin={{ vertical: "top", horizontal: "right" }}
+            >
+                <Alert
+                    severity="error"
+                    onClose={() => {
+                        setNotificationStatus({
+                            ...notificationStatus,
+                            failure: false,
+                        });
+                    }}
+                >
+                    Error when creating question!
+                </Alert>
+            </Snackbar>
+        </Grid>
+    );
+}
+
+export default QuestionAdminPage;
